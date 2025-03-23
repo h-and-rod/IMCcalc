dcbcfc8cb3ab18c2d68d64d5373eec2076505fc2
Henrique Andrade <h.andrade.rodri@gmail.com>
2025-03-23 19:37:20 -0300

    Project's first version

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,8 +10,36 @@ export class FormComponent {
     weight:number = 0;
     height:number = 0;
     IMC:number = 0;
+    imageName: string = "";
 
     onClickIMCCalculateButton(){
       this.IMC = this.weight/(this.height * this.height);
+    }
+
+    IMCClassifing(){
+      if(this.IMC < 18.5) {
+        this.imageName = "minorThan18.png"
+      }
+      
+      else if(this.IMC <= 24.9) {
+        this.imageName = "normal.png"
+      }
+      
+      else if(this.IMC <= 29.9) {
+        this.imageName = "overweight.png"
+      }
+
+      else if(this.IMC <= 34.9) {
+        this.imageName = "obese.png"
+      }
+
+      else if(this.IMC <= 39.9) {
+        this.imageName = "minorThan40.png"
+      }
+
+      else if(this.IMC >= 40) {
+        this.imageName = "majorThan40.png"
+      }
+
     }
 }
